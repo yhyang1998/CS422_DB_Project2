ed6975678ebe23171c855400913cbf2c6bf60f1b
CS 422 TA team <no-email@epfl.ch>
2022-04-27 12:04:30 +0200

    Skeleton code for Project 2

---
+++ src/main/scala/app/recommender/NNLookupWithCache.scala
@@ -0,0 +1,50 @@
+package app.recommender
+
+import org.apache.spark.SparkContext
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
+
+/**
+ * Class for performing LSH lookups (enhanced with cache)
+ *
+ * @param lshIndex A constructed LSH index
+ */
+class NNLookupWithCache(lshIndex : LSHIndex) extends Serializable {
+  var cache = null
+
+  /**
+   * The operation for building the cache
+   *
+   * @param sc Spark context for current application
+   */
+  def build(sc : SparkContext) = ???
+
+  /**
+   * Testing operation: force a cache based on the given object
+   *
+   * @param ext A broadcast map that contains the objects to cache
+   */
+  def buildExternal(ext : Broadcast[Map[IndexedSeq[Int], List[(Int, String, List[String])]]]) = ???
+
+  /**
+   * Lookup operation on cache
+   *
+   * @param queries The RDD of keyword lists
+   * @return The pair of two RDDs
+   *         The first RDD corresponds to queries that result in cache hits and
+   *         includes the LSH results
+   *         The second RDD corresponds to queries that result in cache hits and
+   *         need to be directed to LSH
+   */
+  def cacheLookup(queries: RDD[List[String]])
+  : (RDD[(List[String], List[(Int, String, List[String])])], RDD[(IndexedSeq[Int], List[String])]) = ???
+
+  /**
+   * Lookup operation for queries
+   *
+   * @param input The RDD of keyword lists
+   * @return The RDD of (keyword list, resut) pairs
+   */
+  def lookup(queries: RDD[List[String]])
+  : RDD[(List[String], List[(Int, String, List[String])])] = ???
+}
---
+++ src/main/scala/app/recommender/TitlesLoader.scala
@@ -0,0 +1,20 @@
+package app.recommender
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+/**
+ * Helper class for loading the input
+ *
+ * @param sc The Spark context for the given application
+ * @param path The path for the input file
+ */
+class TitlesLoader(sc : SparkContext, path : String) extends Serializable {
+
+  /**
+   * Read the title file in the given path and convert it into an RDD
+   *
+   * @return The RDD for the given titles
+   */
+  def load(): RDD[(Int, String, List[String])] = ???
+}
